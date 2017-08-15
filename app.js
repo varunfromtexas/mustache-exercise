1e14ed7217c55713f3d906ded2181c96839c0f1a
Varun Ambrose <varunambrose@Varuns-MacBook-Pro.local>
2017-08-15 13:56:59 -0500

    test

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+//Listening on root
+app.get('/todo/', function (req, res) {
+  // TODO write your code here
+})
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
